1895b3415738faad9bcaadd05962373287b20b69
sarveshk4 <sarvesh4347@gmail.com>
2021-10-04 09:49:22 +0530

    checking log

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_log.log"),level=logging.INFO, format=logging_str,filemode="a")
+
+logging.info("Hello, World!")
